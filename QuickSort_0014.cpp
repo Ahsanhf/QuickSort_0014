0257095b15c8e02851613c5a39fcab8f7e1bb32a
ahsanhf <ahsanhf@gmail.com>
2024-03-28 17:01:34 +0700

    Langkah ke 7

--- QuickSort_0014.cpp
+++ QuickSort_0014.cpp
@@ -59,7 +59,8 @@ void q_short(int low, int high)
             i++; //step 6
             cmp_count++;
         }
-
+        cmp_count++;
+        while ((arr[j] > pivot) && (j >= low)) //step 7
     }
 
 
